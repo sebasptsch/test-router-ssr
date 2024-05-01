787f751968f17a248651367253c4249004f427f7
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2024-05-01 15:54:11 +1000

    remove mdx tests

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,25 +1,11 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { SessionConfig, useSession } from 'vinxi/http'
+import { SessionConfig, getRequestIP, useSession } from 'vinxi/http'
 
 async function getData() {
   'use server'
 
-  const sesh = await useSession({
-    password: 'password12345678901011213134esrdtfghbjnkmldcfgvbhjnmkldfgcvhbjnkmldfghbjlkm',
-  } as SessionConfig)
-
-  await sesh.update({
-    user: {
-      id: 1,
-      name: 'John Doe',
-    },
-  })
-
-  const seshUser = await sesh.data
-
-  return new Promise<string>((r) => {
-    setTimeout(() => r('Server: Welcome home!' + JSON.stringify(sesh.data)), 1000)
-  })
+  const ip = getRequestIP()
+  return `Your IP is: ${ip}`
 }
 
 export const Route = createFileRoute('/')({
